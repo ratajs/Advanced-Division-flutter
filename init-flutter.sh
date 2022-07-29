22da5d6a31c986e11384be474c8193d1e4dbb3fd
Šimon (Simon) Rataj <ratajs@users.noreply.github.com>
2022-07-29 10:15:29 +0200

    Add files via upload

--- init-flutter.sh
+++ init-flutter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 flutter create advdiv --org cz.ratajs
-rm -r ./advdiv/lib ./advdiv/android/app/src/main/AndroidManifest.xml ./advdiv/android/app/src/main/res/drawable/launch_background.xml ./advdiv/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-hdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-mdpi/ic_launcher.png ./advdiv/pubspec.yaml ./advdiv/README.md
+rm -r ./advdiv/lib ./advdiv/android/app/src/main/AndroidManifest.xml ./advdiv/android/app/src/main/res/drawable/launch_background.xml ./advdiv/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-hdpi/ic_launcher.png ./advdiv/android/app/src/main/res/mipmap-mdpi/ic_launcher.png ./advdiv/windows/runner/resources/app_icon.ico ./advdiv/pubspec.yaml ./advdiv/README.md
 ln -s ../lib ./advdiv/lib
 ln -s ../../../../../AndroidManifest.xml ./advdiv/android/app/src/main/AndroidManifest.xml
 ln -s ../../../../../../../launch_background.xml ./advdiv/android/app/src/main/res/drawable/launch_background.xml
@@ -9,6 +9,7 @@ ln -s ../../../../../../../icons/144.png ./advdiv/android/app/src/main/res/mipma
 ln -s ../../../../../../../icons/96.png ./advdiv/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
 ln -s ../../../../../../../icons/72.png ./advdiv/android/app/src/main/res/mipmap-hdpi/ic_launcher.png
 ln -s ../../../../../../../icons/48.png ./advdiv/android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+ln -s ../../../../icons/app_icon.ico ./advdiv/windows/runner/resources/app_icon.ico
 ln -s ../pubspec.yaml ./advdiv/pubspec.yaml
 ln -s ../README.md ./advdiv/README.md
 sed -i s/dk7:/dk8:/g ./advdiv/android/app/build.gradle
